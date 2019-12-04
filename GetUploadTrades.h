2675dfc562076bf0b214f818bc2a17c547db23aa
linxiany <lxia@nwimgmt.com>
2019-12-04 11:42:28 -0500

    Enhance GetTrades() function to speed up the function
    Wrote an enhance GetTrades2() to speed up the function.

--- GetUploadTrades.h
+++ GetUploadTrades.h
@@ -3,11 +3,14 @@
 GetUploadTrades.h  Header File
 
 *******************************************/
+void setupTradeUploadFundSql(const char *portlist, int bIn, char *FundSql, int nSqlSize);
 void getAdminUploadTrades(const char* date, const char*countparty, const char *portlist, int bIn);
+void getDateTimeStamp();
+void getTodaysTrades(const char *date, int secs);
 void getUploadMultiFundTrades(const char* date, const char* counterparty, const char *portlist, int bIn);
-void getUploadTrades(const char*, const char*);
 void markUploaded(const char*);
 void markUploadedbyTransNum(int TransNum);
+
 
 #define _GET_UPLOAD_TRADES
 #endif
